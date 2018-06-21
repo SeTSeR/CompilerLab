8006b077fb25ac3113b1f22cd4dc0263548fe2cb
Sergey Makarov <SeTSeR200018@gmail.com>
2018-06-21 20:25:28 +0300

    Added tests for differentiation

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,8 +10,15 @@ test2 = TestCase $ assertEqual "for parsing complex expression: " (Right (Binary
 
 test3 = TestCase $ assertEqual "for parsing erroneous expression: " (Left (MissingParametersError "+")) (runExcept $ parse "2 +")
 
+test4 = TestCase $ assertEqual "simple derivative: " (runExcept $ derivative <$> parse "x 2 +") (runExcept $ parse "1 0 +")
+
+test5 = TestCase $ assertEqual "more complex derivative: " (runExcept $ derivative <$> parse "x sin 3 *") (runExcept $ parse "-1 x cos x cos * / * 3 x sin 3 * +")
+
+test6 = TestCase $ assertEqual "third derivative: " (runExcept $ derivative <$> parse "x x ln *") (runExcept $ parse "1 x ln * x 1 x / * +")
+
 main :: IO ()
 main = do
-    let parserTests = TestList [TestLabel "Test parse simple" test1, TestLabel "Test parse complex" test2, TestLabel "Test parse error" test3]
+    let parserTests = TestList [TestLabel "Test parse simple" test1, TestLabel "Test parse complex" test2, TestLabel "Test parse error" test3,
+                                TestLabel "Test differentiate simple" test4, TestLabel "Test another derivative" test5, TestLabel "Test third rerivative" test6]
     runTestTT parserTests
     return ()
