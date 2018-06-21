11e9f7da00fc3e17d605a3791dd5d9ad48121488
Sergey Makarov <SeTSeR200018@gmail.com>
2018-06-21 22:47:18 +0300

    Added test for division checking

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,13 +12,16 @@ test3 = TestCase $ assertEqual "for parsing erroneous expression: " (Left (Missi
 
 test4 = TestCase $ assertEqual "simple derivative: " (runExcept $ parse "1 0 +") (runExcept $ derivative <$> parse "x 2 +")
 
-test5 = TestCase $ assertEqual "more complex derivative: " (runExcept $ parse "-1 x cos x cos * / * 3 x sin 3 * +") (runExcept $ derivative <$> parse "x sin 3 *")
+test5 = TestCase $ assertEqual "more complex derivative: " (runExcept $ parse "x cos 1 * 3 * x sin 0 * +") (runExcept $ derivative <$> parse "x sin 3 *")
 
-test6 = TestCase $ assertEqual "third derivative: " (runExcept $ parse "1 x ln * x 1 x / * +") (runExcept $ derivative <$> parse "x x ln *")
+test6 = TestCase $ assertEqual "third derivative: " (runExcept $ parse "1 x ln * x 1 x / 1 * * +") (runExcept $ derivative <$> parse "x x ln *")
+
+test7 = TestCase $ assertEqual "test case with divide: " (runExcept $ parse "-1 x sin * e x ^ * x cos e x ^ * 1 * - e x ^ e x ^ * /") (runExcept $ derivative <$> parse "x cos e x ^ /")
 
 main :: IO ()
 main = do
     let parserTests = TestList [TestLabel "Test parse simple" test1, TestLabel "Test parse complex" test2, TestLabel "Test parse error" test3,
-                                TestLabel "Test differentiate simple" test4, TestLabel "Test another derivative" test5, TestLabel "Test third rerivative" test6]
+                                TestLabel "Test differentiate simple" test4, TestLabel "Test another derivative" test5, TestLabel "Test third rerivative" test6,
+                                TestLabel "Test with exponent and division" test7]
     runTestTT parserTests
     return ()
