01a109d164addf819812126372e8905eacf3bd3c
Sergey Makarov <SeTSeR200018@gmail.com>
2018-06-26 12:06:33 +0300

    Printing errors to stderr

--- compiler-rust/src/main.rs
+++ compiler-rust/src/main.rs
@@ -9,12 +9,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("There was a problem during parsing arguments: {}", err);
+        eprintln!("There was a problem during parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = compiler_rust::run(config) {
-        println!("There were an errors during compilation: {}", e);
+        eprintln!("There were an errors during compilation: {}", e);
         process::exit(1);
     }
 }
